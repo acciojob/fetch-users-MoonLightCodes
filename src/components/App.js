d40e9686d39a8f24569153e864104531377592e4
PoornachanderReddy <poornachander295@gmail.com>
2025-05-17 23:39:02 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,9 @@ import React, { useCallback, useEffect, useState } from "react";
 import "./../styles/App.css";
 
 const App = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
   const getData = useCallback(() => {
-     fetch("https://reqres.in/api/users", {
+    fetch("https://reqres.in/api/users", {
       method: "GET",
       headers: {
         "x-api-key": "reqres-free-v1",
@@ -15,7 +15,7 @@ const App = () => {
       })
       .then((d) => setData(d.data));
   }, []);
-  useEffect(getData,[])
+  useEffect(getData, []);
   console.log(data);
   return (
     <div>
@@ -34,17 +34,28 @@ const App = () => {
             <th>Avatar</th>
           </tr>
         </thead>
-        <tbody style={{textAlign:"center"}}>{data?.map((ele)=>{
-          return (
-            <tr key={ele.email}>
-              <td>{ele.first_name}</td>
-              <td>{ele.last_name}</td>
-              <td>{ele.email}</td>
-              <td><img src={ele.avatar} alt="AVARAT" style={{width:"2rem",height:"2rem"}} /></td>
-            </tr>
-          );
-        })}</tbody>
+        {data.length !== 0 && (
+          <tbody style={{ textAlign: "center" }}>
+            {data?.map((ele) => {
+              return (
+                <tr key={ele.email}>
+                  <td>{ele.first_name}</td>
+                  <td>{ele.last_name}</td>
+                  <td>{ele.email}</td>
+                  <td>
+                    <img
+                      src={ele.avatar}
+                      alt="AVARAT"
+                      style={{ width: "2rem", height: "2rem" }}
+                    />
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        )}
       </table>
+      {data.length === 0 &&<h1 style={{textAlign:"center"}}>No data found to display.</h1>}
     </div>
   );
 };
